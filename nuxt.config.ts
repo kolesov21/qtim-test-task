204822d71524214f97cccd7d7f9ae64fbcd25347
Dmitriy Kolesov <kolesov@ramm.group>
2025-09-01 13:44:09 +0500

    Настройка и подключение tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   devtools: { enabled: false },
+  css: ['~/assets/styles/main.css'],
   compatibilityDate: '2025-07-15',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   typescript: {
     strict: true,
   },
